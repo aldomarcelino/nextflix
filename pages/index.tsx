2583460da10b236e5d98d65baddb501bbe0fa68b
Aldo Marcelino <aldo115marcelino@gmail.com>
2024-01-04 22:59:48 +0700

    add sign up

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,23 @@ import { NextRouter, useRouter } from 'next/router';
 import styles from '../styles/Login.module.scss';
 import LoginBg from '../public/assets/loginBg.jpg';
 import { ROUTES } from '../config/route';
+import { useEffect, useState } from 'react';
 
 export default function Home(): React.ReactElement {
   const router: NextRouter = useRouter();
+  const status = router.query.status || '';
+
+  const [isShow, setIsShow] = useState(false);
+  const [isRegisterd, setIsregisterd] = useState(false);
+
+  useEffect(() => {
+    if (isRegisterd) {
+      setTimeout(() => {
+        setIsregisterd(false);
+        router.push('/');
+      }, 3000);
+    }
+  }, [isRegisterd]);
 
   const onSignIn = () => {
     router.push(ROUTES.BROWSE);
@@ -23,17 +37,52 @@ export default function Home(): React.ReactElement {
 
       <main className={styles.main}>
         <Image src={LoginBg} alt='background image' placeholder='blur' layout='fill' className={styles.main__bgImage} />
-        <div className={styles.main__card}>
-          <h1>Nextflix</h1>
-          <div className={styles.wrapInput}>
-            <input className={styles.textField} placeholder='Email or Phone Number' type='text' />
-            <input className={styles.textField} placeholder='Password' type='password' />
+        {status !== 'signup' ? (
+          <div className={styles.main__card}>
+            <h1>Nextflix</h1>
+            <div className={styles.wrapInput}>
+              <input className={styles.textField} placeholder='Email or Phone Number' type='text' />
+              <input className={styles.textField} placeholder='Password' type='password' />
+            </div>
+            <p>A simple Netflix clone built using Next.js</p>
+            <div className={styles.button} onClick={onSignIn}>
+              Sign in
+            </div>
+            <div className={styles.signUp} onClick={() => router.push('/?status=signup')}>
+              Sign Up
+            </div>
           </div>
-          <p>A simple Netflix clone built using Next.js</p>
-          <div className={styles.button} onClick={onSignIn}>
-            Sign in
+        ) : (
+          <div className={styles.containSecond}>
+            {isRegisterd ? (
+              <p className={styles.message}>Your Account Registered Succesfully You Can Now Sign In with it</p>
+            ) : (
+              <>
+                <h1>Sign Up</h1>
+                <div className={styles.wrapInput}>
+                  <input className={styles.textField} placeholder='Name' type='text' />
+                  <input className={styles.textField} placeholder='Email' type='text' />
+                  <input className={styles.textField} placeholder='Phone Number' type='text' />
+                  <input className={styles.textField} placeholder='Password' type={isShow ? 'test' : 'password'} />
+                  <input className={styles.textField} placeholder='Re Password' type={isShow ? 'test' : 'password'} />
+                </div>
+                <p className={styles.showpass} onClick={() => setIsShow(!isShow)}>
+                  {isShow ? 'Hide Password' : 'Show Password'}
+                </p>
+              </>
+            )}
+            {!isRegisterd && (
+              <>
+                <div className={styles.button} onClick={() => setIsregisterd(true)}>
+                  Submit
+                </div>
+                <div className={styles.signUp} onClick={() => router.push('/')}>
+                  Sign In
+                </div>
+              </>
+            )}
           </div>
-        </div>
+        )}
       </main>
     </div>
   );
